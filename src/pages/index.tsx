0bbb4659b754299c8fd9dc7f6a59aa06127d4df1
Gabriel Vicente <vicente.gabrielv@gmail.com>
2022-10-17 13:59:18 -0300

    Success page complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { stripe } from "../lib/stripe";
 import { GetStaticProps } from "next";
 import Stripe from "stripe";
 import Link from "next/link";
+import Head from "next/head";
 
 type Product = {
   id: string;
@@ -29,20 +30,26 @@ export default function Home({ products }: HomeProps) {
 
 
   return (
-    <HomeContainer ref={sliderRef} className='keen-slider'>
-      {products.map(product => (
-        <Link key={product.id} href={`/product/${product.id}`} prefetch={false} >
-          <Product  className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt='camiseta1' />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
-        </Link>
-        
-      ))}
-    </HomeContainer>
+    <>
+      <Head>
+        <title>Home - Ignite Shop</title>
+      </Head>
+
+      <HomeContainer ref={sliderRef} className='keen-slider'>
+        {products.map(product => (
+          <Link key={product.id} href={`/product/${product.id}`} prefetch={false} >
+            <Product  className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt='camiseta1' />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
+        ))}
+      </HomeContainer>
+    </>
+    
   )
 }
 
